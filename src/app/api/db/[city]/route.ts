a238dc1e006397f89afd4aad70b146014d74a4bc
Arthas007 <arthaszs007@gmail.com>
2024-04-19 01:07:04 -0600

    5times

--- src/app/api/db/[city]/route.ts
+++ src/app/api/db/[city]/route.ts
@@ -22,8 +22,14 @@ export async function GET(req:Request, {params}:{params:{city:string}}) {
 
     // const { rows }  = await client.query("SELECT * FROM location");
 
-    
-    return new Response(JSON.stringify(rows), { status: 200 });
+    const headers = {
+      'Access-Control-Allow-Origin': '*', // 允许所有来源跨域访问，也可以指定特定的来源
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS', // 允许的 HTTP 方法
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization', // 允许的请求头
+      'Access-Control-Allow-Credentials': 'true', // 是否允许发送 Cookie
+    };
+
+    return new Response(JSON.stringify(rows), { status: 200,headers:headers });
 
 
   } finally {
